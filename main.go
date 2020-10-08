5b9797f9a09c652a1ce4aaff14436cde16c3c91e
hiinnn <hin_pansa@hotmail.com>
2020-10-08 15:13:03 +0700

    first commit

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+func main() {
+	r := gin.Default()
+	dsn := "user=gorm password=gorm dbname=gorm port=9920 sslmode=disable TimeZone=Asia/Shanghai"
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+
+	fmt.Print(r, db, err)
+}
